347742abb140eeaddc68b8d0007d7afd530ce5a5
Carissa Castro <carissabcastro@gmail.com>
2017-10-25 11:55:56 -0500

    Initial commit

---
+++ nancy.rb
@@ -0,0 +1,53 @@
+require "rack"
+
+module Nancy
+  class Base
+    def initialize
+      @routes = {}
+    end
+
+    def call(env)
+      @request = Rack::Request.new(env)
+      verb = @request.request_method
+      requested_path = @request.path_info
+      handler = @routes.fetch(verb, {}).fetch(requested_path, nil)
+
+      if handler
+        instance_eval(@handler)
+      else
+        [404, {}, ["Oops! No route for #{verb} #{requested_path}"]]
+      end
+    end
+
+    attr_reader :routes
+
+    def get(path, &handler)
+      route("GET", path, &handler)
+    end
+
+    def params
+      @request.params
+    end
+
+    private
+
+    def route(verb, path, &handler)
+      @routes[verb] ||= {}
+      @routes[verb][path] = handler
+    end
+  end
+end
+
+nancy = Nancy::Base.new
+
+nancy.get "/hello" do
+  [200, {}, ["Nancy says hello"]]
+end
+
+nancy.get "/" do
+  [200, {}, ["Your params are #{params.inspect}"]]
+end
+
+puts nancy.routes
+
+Rack::Handler::WEBrick.run nancy, Port: 9292
