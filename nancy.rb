263a45af52f8d86674cd016c903cc05407380020
Carissa Castro <carissabcastro@gmail.com>
2017-10-25 12:51:48 -0500

    Project complete

--- nancy.rb
+++ nancy.rb
@@ -57,22 +57,56 @@ module Nancy
       @routes[verb][path] = handler
     end
   end
+
+  Application = Base.new
+
+  module Delegator
+    def self.delegate(*methods, to:)
+      Array(methods).each do |method_name|
+        define_method(method_name) do |*args, &block|
+          to.send(method_name, *args, &block)
+        end
+
+        private method_name
+      end
+    end
+
+    delegate :get, :patch, :put, :post, :delete, :head, to: Application
+  end
 end
 
-nancy = Nancy::Base.new
+include Nancy::Delegator
 
-nancy.get "/hello" do
-  "Nancy says hello"
+# nancy = Nancy::Base.new
+
+# nancy.get "/hello" do
+#   "Nancy says hello"
+# end
+#
+# nancy.get "/" do
+#   [200, {}, ["Your params are #{params.inspect}"]]
+# end
+#
+# nancy.post "/" do
+#   [200, {}, request.body]
+# end
+
+# nancy_application = Nancy::Application
+#
+# nancy_application.get "/hello" do
+#   "Nancy::Application says hello"
+# # end
+#
+# puts nancy.routes
+#
+# Rack::Handler::WEBrick.run nancy_application, Port: 9292
+
+get "/bare-get" do
+  "Whoa, it works!"
 end
 
-nancy.get "/" do
-  [200, {}, ["Your params are #{params.inspect}"]]
+post "/" do
+  request.body.read
 end
 
-nancy.post "/" do
-  [200, {}, request.body]
-end
-
-puts nancy.routes
-
-Rack::Handler::WEBrick.run nancy, Port: 9292
+Rack::Handler::WEBrick.run Nancy::Application, Port: 9292
